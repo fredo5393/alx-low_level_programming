d4e1ab7ea10c5ff5fff14e16796f568cd18dd294
fredo5393 <fredo5393@gmail.com>
2023-03-16 03:28:48 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Printing a program with puts function
+ * Return: 0 (Successfull)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art");
+return (0);
+}
