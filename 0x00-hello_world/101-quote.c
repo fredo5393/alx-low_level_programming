d89e4305c493aa1aea3e917bb58e36974a289f21
fredo5393 <fredo5393@gmail.com>
2023-03-16 14:59:50 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Printing a program with printf function
+ * Return: 1 (Successfull)
+ */
+int main(void)
+{
+	write(2,
+			"and that piece of art is useful\n" "- Dora Korpar, 2015-10-19\n");
+	return (1);
+}
