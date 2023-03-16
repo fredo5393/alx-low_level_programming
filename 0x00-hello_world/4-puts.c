f649a9b7e62a46cf6bed91b0424ee514d8c0da96
fredo5393 <fredo5393@gmail.com>
2023-03-16 03:22:12 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Printing a program with puts function
+ * Return: 0 (Successfull)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
